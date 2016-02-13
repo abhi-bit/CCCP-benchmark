6b1485266f006971c20759c4478e1b4125739256
Abhishek Singh <abhishek@couchbase.com>
2016-02-13 16:52:15 +0530

    minor tweaks

--- perf_runner.py
+++ perf_runner.py
@@ -3,11 +3,13 @@
 import datetime
 import multiprocessing
 from rest import Rest
+import sys
 import yaml
 
 CONFIG = yaml.load(file('config.yaml', 'rb'))
 
-CALL_COUNT_PER_CHILD = 1000
+CALL_COUNT_PER_CHILD = int(sys.argv[1])
+PARALLELISM = int(sys.argv[2])
 
 
 def do_config_call(ip, admin_port, mcd_port):
@@ -24,7 +26,7 @@ def main():
     mcd_port = int(cbconf['mcd_port'])
 
     jobs = []
-    pool_size = multiprocessing.cpu_count() / 2
+    pool_size = multiprocessing.cpu_count() * PARALLELISM
     a = datetime.datetime.now().replace(microsecond=0)
     for i in xrange(pool_size):
         p = multiprocessing.Process(target=do_config_call, args=(ip,
@@ -37,6 +39,7 @@ def main():
         p.join()
 
     b = datetime.datetime.now().replace(microsecond=0)
+    print "Fired %d CCCP calls" % (CALL_COUNT_PER_CHILD * pool_size)
     print "Time elapsed: %s sec" % (b-a)
 
 if __name__ == "__main__":
